465488e4d25312d3e5ebb3d3b542bbe103ac4b1b
Arthur Aires Vieira <arthur.ifpb@gmail.com>
2024-12-11 17:16:48 -0300

    ajuste de comentarios

--- src/MiddlewareServiceProvider.php
+++ src/MiddlewareServiceProvider.php
@@ -8,7 +8,6 @@ class MiddlewareServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Torna as migrations publicáveis
         $this->publishes([
             __DIR__.'/../database/migrations/' => database_path('migrations'),
         ], 'middleware-migrations');
